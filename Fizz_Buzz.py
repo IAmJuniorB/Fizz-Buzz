c9bfd8b65e66d8499b6e0d2304ccf9e89ad887b2
Joe Bonfanti Jr <57663362+IAmJuniorB@users.noreply.github.com>
2023-01-25 11:19:22 -0500

    Add files via upload

---
+++ Fizz_Buzz.py
@@ -0,0 +1,12 @@
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    elif input % 3 == 0:
+        return "Fizz"
+    elif input % 5 == 0:
+        return "Buzz"
+    else:
+        return (f"{input} is not divisable by 3 or 5")
+
+
+print(fizz_buzz(15))
